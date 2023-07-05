8d69bccb4600c0f663050583201a3f194afe017d
Edwin Melara <edwin.landaverde@salud.gob.sv>
2023-07-05 11:32:58 -0600

    Primer test(Status)

---
+++ tests/Feature/CreateStatusTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CreateStatusTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function un_usuario_auteticado_puede_crear_estado(): void
+    {
+        //$this->withoutExceptionHandling();
+        // 1. Given => Teniendo un usuario autenticado
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        // 2. When => Cuando hace un post request a status
+        $response = $this->post(route('statuses.store'), ['body' => 'Mi primer estado']);
+        // 3. Then => Entonces veo un nuevo estado en la base de datos
+        $this->assertDatabaseHas('statuses', [
+            'user_id' => $user->id,
+            'body' => 'Mi primer estado'
+        ]);
+    }
+}
